c4084eafb78b49fdcf84d9a3035670ca32ea8ea6
kemaltulum <tulum.ke@gmail.com>
2020-05-22 17:19:48 +0300

    Remove config file

--- web-2018-w4/dist/config.js
+++
@@ -1,8 +0,0 @@
-var config = {
-    apiKey: "AIzaSyCBk4HUnOdepq9a_YCeHa2gfUw5283tYD4",
-    authDomain: "compec-yemek.firebaseapp.com",
-    databaseURL: "https://compec-yemek.firebaseio.com",
-    projectId: "compec-yemek",
-    storageBucket: "compec-yemek.appspot.com",
-    messagingSenderId: "751823870312"
-};
